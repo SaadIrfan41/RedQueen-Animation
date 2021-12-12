acd45cf704ec611db71253e8557946fb9b0b3100
saix511 <saadirfan41@gmail.com>
2021-12-12 18:58:36 +0500

    project completed

---
+++ src/components/Redqueen.js
@@ -0,0 +1,168 @@
+import { useEffect } from 'react'
+import useWebAnimations from '@wellyshen/use-web-animations'
+
+const Redqueen = () => {
+  const sceneryFrames = [
+    { transform: 'translateX(100%)' },
+    { transform: 'translateX(-100%)' },
+  ]
+  const sceneryTimingBackground = {
+    duration: 36000,
+    iterations: Infinity,
+  }
+
+  const sceneryTimingForeground = {
+    duration: 12000,
+    iterations: Infinity,
+  }
+
+  const background1Movement = useWebAnimations({
+    keyframes: sceneryFrames,
+    timing: sceneryTimingBackground,
+  })
+  const background2Movement = useWebAnimations({
+    keyframes: sceneryFrames,
+    timing: sceneryTimingBackground,
+  })
+
+  const foreground1Movement = useWebAnimations({
+    keyframes: sceneryFrames,
+    timing: sceneryTimingForeground,
+  })
+
+  const foreground2Movement = useWebAnimations({
+    keyframes: sceneryFrames,
+    timing: sceneryTimingForeground,
+  })
+
+  const redQueen_alice = useWebAnimations({
+    keyframes: [
+      { transform: 'translateY(0)' },
+      { transform: 'translateY(-100%)' },
+    ],
+    timing: {
+      easing: 'steps(7, end)',
+      direction: 'reverse',
+      duration: 600,
+      playbackRate: 1,
+      iterations: Infinity,
+    },
+  })
+
+  useEffect(() => {
+    const sceneries = [
+      background1Movement.getAnimation(),
+      background2Movement.getAnimation(),
+      foreground1Movement.getAnimation(),
+      foreground2Movement.getAnimation(),
+    ]
+    const adjustBackgroundPlayback = () => {
+      if (redQueen_alice.getAnimation().playbackRate < 0.8) {
+        sceneries.forEach(function (animate) {
+          animate.playbackRate =
+            redQueen_alice?.getAnimation()?.playbackRate * 2
+        })
+      } else if (redQueen_alice.getAnimation().playbackRate > 1.2) {
+        sceneries.forEach(function (animate) {
+          animate.playbackRate =
+            redQueen_alice?.getAnimation()?.playbackRate * 2.5
+        })
+      }
+    }
+
+    setInterval(() => {
+      if (redQueen_alice.getAnimation().playbackrateRQ > 0.4) {
+        redQueen_alice.getAnimation().playbackRate *= 0.9
+      }
+      adjustBackgroundPlayback()
+    }, 3000)
+
+    document.addEventListener('click', () => {
+      redQueen_alice
+        .getAnimation()
+        .updatePlaybackRate(redQueen_alice.getAnimation().playbackRate * 1.1)
+      adjustBackgroundPlayback()
+    })
+  })
+
+  return (
+    <div className='wrapper'>
+      <div className='sky' />
+      <div className='earth'>
+        <div id='red-queen_and_alice'>
+          <img
+            id='red-queen_and_alice_sprite'
+            src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png'
+            srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x'
+            alt='Alice and the Red Queen running to stay in place.'
+            ref={redQueen_alice.ref}
+          />
+        </div>
+      </div>
+      <div className='scenery' id='foreground1' ref={foreground1Movement.ref}>
+        <img
+          id='palm3'
+          src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png'
+          srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x'
+          alt=' '
+        />
+      </div>
+      <div className='scenery' id='foreground2' ref={foreground2Movement.ref}>
+        <img
+          id='bush'
+          src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png'
+          srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x'
+          alt=' '
+        />
+        <img
+          id='w_rook_upright'
+          src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png'
+          srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x'
+          alt=' '
+        />
+      </div>
+      <div className='scenery' id='background1' ref={background1Movement.ref}>
+        <img
+          id='r_pawn_upright'
+          src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png'
+          srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x'
+          alt=' '
+        />
+        <img
+          id='w_rook'
+          src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png'
+          srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x'
+          alt=' '
+        />
+        <img
+          id='palm1'
+          src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png'
+          srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x'
+          alt=' '
+        />
+      </div>
+      <div className='scenery' id='background2' ref={background2Movement.ref}>
+        <img
+          id='r_pawn'
+          src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png'
+          srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x'
+          alt=' '
+        />
+        <img
+          id='r_knight'
+          src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png'
+          srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x'
+          alt=' '
+        />
+        <img
+          id='palm2'
+          src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png'
+          srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x'
+          alt=' '
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Redqueen
